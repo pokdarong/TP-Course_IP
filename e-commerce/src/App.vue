92e0d982730926e71c6ad1d948319e8bd7eb8f3c
pokdarong <podarong@gmail.com>
2024-10-21 12:29:45 +0700

    finish

--- e-commerce/src/App.vue
+++ e-commerce/src/App.vue
@@ -4,20 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-  
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<div>TP1-Internet</div>
 </template>
 
 <style scoped>
